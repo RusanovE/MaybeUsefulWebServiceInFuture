fe973561c77c8e861679cd26cda115ad8f19f898
RusanovE <112477924+RusanovE@users.noreply.github.com>
2024-02-09 22:37:49 +0200

    Update UserDTO.java

--- MushroomsDetect/src/main/java/com/example/mushroomsdetect/DTO/UserDTO.java
+++ MushroomsDetect/src/main/java/com/example/mushroomsdetect/DTO/UserDTO.java
@@ -1,13 +1,16 @@
 package com.example.mushroomsdetect.DTO;
 
+import com.example.mushroomsdetect.entitys.Role;
 import lombok.Builder;
 import lombok.Data;
+import org.springframework.web.multipart.MultipartFile;
 
 @Builder
 @Data
 public class UserDTO {
     String login;
     String password;
-    String role;
-    byte[] photo;
+    Role role;
+    MultipartFile photo;
 }
+
